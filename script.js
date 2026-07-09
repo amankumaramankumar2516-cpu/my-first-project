cbe9e5f085acf1b0ae2e40a456bd96e604ed96e1
amankumaramankumar2516-cpu <amankumaramankumar2516@gmail.com>
2026-07-10 01:34:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.querySelector("button");
+document.addEventListener("DOMContentLoaded", function () {
 
-  btn.addEventListener("click", () => {
-    alert("Welcome to PresentAI Pro!");
-  });
+  const startBtn = document.getElementById("startBtn");
+  const loginBtn = document.getElementById("loginBtn");
+
+  if (startBtn) {
+    startBtn.addEventListener("click", function () {
+      alert("Welcome to PresentAI Pro!");
+
+      // AI Generator section par le jayega
+      document.querySelector(".cards").scrollIntoView({
+        behavior: "smooth"
+      });
+    });
+  }
+
+  if (loginBtn) {
+    loginBtn.addEventListener("click", function () {
+      alert("Login page is coming soon!");
+    });
+  }
 
   document.querySelectorAll("nav a").forEach(link => {
-    link.addEventListener("click", (e) => {
+    link.addEventListener("click", function(e) {
       e.preventDefault();
-      alert(link.textContent + " page is coming soon!");
+      alert(this.textContent + " page is coming soon!");
     });
   });
+
 });
